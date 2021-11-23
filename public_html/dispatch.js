6e261fb1f1332b0d7a0b2a18bbe13a21a870e383
kjstrongman <60051044+kjstrongman@users.noreply.github.com>
2021-11-22 18:26:30 -0700

    Working GeoMap

--- public_html/dispatch.js
+++ public_html/dispatch.js
@@ -1,39 +1,10 @@
+//dispatch.js 
 
-function buildMsgHist(result) {
-  let msgBox = '';
-  // Creates JSON format of message
-  let messageArray = JSON.parse(result);
-  // Iterates all messages
-  for (i in messageArray) {
-    // Selects message
-    let msg = messageArray[i];
-    // Adds to box
-    msgBox += `<b>${msg.alias}</b>: ${msg.message}<br>`;
-  }
-  $('#getDispatchMsg').html(msgBox); // Add to html
-}
-
-/*
-Executes every second to check for new messages
-*/
-function update() {
-  $.ajax({
-    url: '/chat',
-    method: 'GET',
-    success: (result) => buildMsgHist(result)
-  });
-}
-
-/*
-This function posts new messages to the chat box.
-*/
-function send() {
-  $.ajax({
-    url: '/chat/post',
-    data: { alias: $('#alias').val(),
-      msg: $('#message').val() },
-    method: 'POST'
-  });
-}
-// Update interval
-setInterval(() => update(), 1000);
+$(function getMap() {
+    // create a basic map centered on Tucson
+    // at zoom-level 11
+    $("#map").geomap({
+        center: [-110.911789, 32.253460], //long, lat in decimal form
+        zoom: 11,
+    });
+});
---
+++ public_html/home.js
@@ -0,0 +1,10 @@
+//home.js 
+
+$(function getMap() {
+    // create a basic map centered on Tucson
+    // at zoom-level 11
+    $("#map").geomap({
+        center: [-110.911789, 32.253460], //long, lat in decimal form
+        zoom: 11,
+    });
+});
