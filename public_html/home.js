3f519a5d6cfad8bc77ccd05228d4390a961414a7
kjstrongman <60051044+kjstrongman@users.noreply.github.com>
2021-12-02 09:46:58 -0700

    Pin display features enabled

--- public_html/home.js
+++ public_html/home.js
@@ -1,7 +1,12 @@
 /*
+File: home.js
+Purpose: General users can report and view
+reports on the map.
+Supports index.html functions. 
+
+
 This creates pins and updates the map for the general users
 */
-
 $(function getMap() {
     // create a basic map centered on Tucson
     // at zoom-level 11
@@ -13,26 +18,17 @@ $(function getMap() {
             find: "point",
         },
         click: positionEventHandler,
-
-
-
     });
     // This function displays the coordinates once user clicks
     function positionEventHandler(e, geo) {
-
-
         map.geomap("append", geo)
         var displayCoords = geo.coordinates;
-
         //updates the DOM with lat and long from clicking
         elements = "<div id='coords'> <b>Coordinates:</b> </br><label>Latitude<input id='lat' value=" + displayCoords[1] +
             "> </label><br><label>Longitude<input id='long' value=" + displayCoords[0] + "> " +
             "</label><br>"
         $('#coords').html(elements)
-
     }
-
-
 });
 
 /*
@@ -49,7 +45,69 @@ function createReport() {
             description: $('#report').val(),
             deparment: $('department').val(),
             time: Date.now()
+        },
+        success: (result) => updateMap()
+    });
+};
+/*
+This function displays the pin information on the map
+Result is the JSON of pins from the server/db
+*/
+function displayPin(result) {
+    var map = $("#map").geomap({
+        cursors: {
+            find: "point",
+        },
+        move: function (e, geo) {
+            // search for a pin under the cursor
+            var monument = map.geomap("find", geo, 12);
+            if (monument.length > 0) {
+                // if found, show its label
+                $("." + monument[0].properties.id).closest(".geo-label").show();
+            } else {
+                // otherwise, hide all labels
+                $(".geo-label").hide();
+            }
         }
     });
+    let pinArray = JSON.parse(result);
+    let pins = []
+    //Creates a JSONgeo pin to display information on the page
+    for (i in pinArray) {
+        let pin = pinArray[i]
+        var monuments = {
+            type: "Feature",
+            geometry: {
+                type: "Point",
+                coordinates: [pin.long, pin.lat]
+            },
+            properties: {
+                id: pin.id,
+                title: pin.title,
+                description: pin.description,
+                department: pin.department,
+                time: pin.time,
+            }
+        };
+        pins.push(monuments);
+    };
+    $.each(pins, function () {
+        // label has a class that matches the pin's id
+        // false (at the end) stops jQuery Geo from trying to refresh after each append
+        map.geomap("append", this, '<span class="' + this.properties.id + '"> Title: ' + this.properties.title +
+            '<br>id: ' + this.properties.id + '<br>Description: ' + this.properties.description + '<br>Department: '
+            + this.properties.department + '</span>', false);
+    });
+    //refresh the map
+    //map.geomap("refresh");
+};
+//Calls for pin updates from the server/db. 
+function updateMap() {
+    $.ajax({
+        url: '/test',
+        method: 'GET',
+        success: (result) => displayPin(result)
+    });
 }
-
+//Interval to check for pin updates
+setInterval(() => updateMap(), 10000);
