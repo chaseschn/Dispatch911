b1c1c23772b2fefe9e12b720b4d491925c065850
andrew0jx <92968156+andrew0jx@users.noreply.github.com>
2021-12-05 20:42:08 -0700

    log in and create an account
    to do: fix function, alerts, possible token glitch

--- public_html/account/registration.js
+++ public_html/account/registration.js
@@ -1,18 +1,34 @@
 /*
 Adds a user to the DB.
 */
-function addUser() {
-  $.ajax({
-    url: '/add/user',
-    method: 'POST',
-    data: {
-      fname: $('#fname').val(),
-      lname: $('#lname').val(),
-      email: $('#email').val(),
-      password: $('#email').val(),
-      phone: $('#password').val(),
-      function: $('#function').val(),
-      unit: $('#unit').val()
-    }
+//TO FIX: Function saves as undefined
+function Login() {
+  let email = $('#emailLogin').val();
+  let password = $('#passwordLogin').val();
+  $.get( //not sure how to do this better
+   '/account/login/' + encodeURIComponent(email) + '/' + encodeURIComponent(password),
+    (data, status) => {
+      if (data == 'Login Successful') {
+        window.location.href = '/home.html';
+      }
   });
+}
+
+function createAccount() {
+  let fname = $('#fname').val();
+  let lname = $('#lname').val();
+  let email = $('#email').val();
+  let password = $('#password').val();
+  let phone = $('#phone').val();
+  let functionVar = $('#functionVar').val();
+  let unit = $('#unit').val();
+  $.post( //not sure how to do this better
+   '/account/create/' + fname + '/' + lname + '/' + encodeURIComponent(email) + '/' + encodeURIComponent(password) + '/' + encodeURIComponent(phone)+ '/' + functionVar + '/' + encodeURIComponent(unit),
+    (data, status) => {
+      alert(data);
+      if (data == 'Account Created') {
+        window.location.href = '/account/index.html';
+      }
+  });
+  
 }
